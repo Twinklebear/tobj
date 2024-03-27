fb5645c76ae73e89197ed2b0957f902e4eaa8704
Josh Lengel <jlengel12@gmail.com>
2024-03-27 01:45:18 +0100

    Fixed type error
    The argument of mtllib is no longer wrapped by Option. The control logic has been adjusted accordingly

--- src/lib.rs
+++ src/lib.rs
@@ -1743,25 +1743,22 @@ where
             }
             Some("mtllib") => {
                 // File name can include spaces so we cannot rely on a SplitWhitespace iterator
-                if let Some(mtllib) = line.split_once(' ').unwrap_or_default().1.trim() {
-                    let mat_file = Path::new(mtllib).to_path_buf();
-                    match material_loader(mat_file.as_path()) {
-                        Ok((mut mats, map)) => {
-                            // Merge the loaded material lib with any currently loaded ones,
-                            // offsetting the indices of the appended
-                            // materials by our current length
-                            let mat_offset = materials.len();
-                            materials.append(&mut mats);
-                            for m in map {
-                                mat_map.insert(m.0, m.1 + mat_offset);
-                            }
-                        }
-                        Err(e) => {
-                            mtlresult = Err(e);
+                let mtllib = line.split_once(' ').unwrap_or_default().1.trim();
+                let mat_file = Path::new(mtllib).to_path_buf();
+                match material_loader(mat_file.as_path()) {
+                    Ok((mut mats, map)) => {
+                        // Merge the loaded material lib with any currently loaded ones,
+                        // offsetting the indices of the appended
+                        // materials by our current length
+                        let mat_offset = materials.len();
+                        materials.append(&mut mats);
+                        for m in map {
+                            mat_map.insert(m.0, m.1 + mat_offset);
                         }
                     }
-                } else {
-                    return Err(LoadError::MaterialParseError);
+                    Err(e) => {
+                        mtlresult = Err(e);
+                    }
                 }
             }
             Some("usemtl") => {
